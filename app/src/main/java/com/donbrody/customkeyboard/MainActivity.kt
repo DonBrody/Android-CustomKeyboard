92618b14a27bef7588ac34cc198e2597a845d72e
IN-LT-79 <kiran.shaw@quantuminventions.com>
2022-01-20 12:54:46 +0530

    formatted file

--- app/src/main/java/com/donbrody/customkeyboard/MainActivity.kt
+++ app/src/main/java/com/donbrody/customkeyboard/MainActivity.kt
@@ -22,17 +22,23 @@ class MainActivity : AppCompatActivity() {
         val qwertyField: EditText = findViewById(R.id.testQwertyField)
 
         keyboard = findViewById(R.id.customKeyboardView)
-         keyboard.setDecimalSeparator(',') // This is for applying localisation
-         keyboard.setThousandSeparator('.') // This is for applying localisation
+        keyboard.setDecimalSeparator(',') // This is for applying localisation
+        keyboard.setThousandSeparator('.') // This is for applying localisation
         keyboard.registerEditText(CustomKeyboardView.KeyboardType.NUMBER, numberField)
-        keyboard.registerEditText(CustomKeyboardView.KeyboardType.NUMBER_DECIMAL, numberDecimalField)
+        keyboard.registerEditText(
+            CustomKeyboardView.KeyboardType.NUMBER_DECIMAL,
+            numberDecimalField
+        )
         keyboard.registerEditText(CustomKeyboardView.KeyboardType.QWERTY, qwertyField)
 
         val switchActivitiesButton: Button = findViewById(R.id.switchActivitiesButton)
         switchActivitiesButton.setOnClickListener {
-            startActivity(Intent(
+            startActivity(
+                Intent(
                     this@MainActivity,
-                    AdvancedFeaturesActivity::class.java))
+                    AdvancedFeaturesActivity::class.java
+                )
+            )
         }
     }
 
